46bb4fa4eaa06b37931cf4e06136bfc4a971a794
nilesh-chothave <nileshchothave2003@gmail.com>
2026-07-19 23:24:41 +0530

    Uploaded C++ Programming Files

---
+++ Practice/program161.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+#pragma pack(1)
+class ArrayX
+{
+    public:
+        int *Arr;
+        int iSize;
+
+        ArrayX(int X)
+        {   
+            iSize = X;
+            Arr = new int[iSize];
+        }
+
+        ~ArrayX()
+        {
+            cout<<"Inside destructor\n";
+
+            delete []Arr;
+        }
+};
+
+int main()
+{
+
+    ArrayX *aboj1 = new ArrayX(5);
+
+    // Logic (Function call)
+
+    delete aboj1;
+
+    return 0;
+}
