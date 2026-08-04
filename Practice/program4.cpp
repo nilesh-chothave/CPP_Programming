e02c350a146eb8127be426d59b087dedcfffd2e0
nilesh-chothave <nileshchothave2003@gmail.com>
2026-08-04 21:23:38 +0530

    C++ Programming Practice Files Uploaded.

---
+++ Practice/program4.cpp
@@ -0,0 +1,23 @@
+/*
+    Addition of two number
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i, j, k;
+
+    cout<<"Enter the first number :  \n";
+    cin>>i;
+
+    cout<<"Enter the sceond number : \n";
+    cin>>j;
+
+    k = i + j;
+
+    cout<<"Addition is : \n"<<k;
+
+    return 0;
+}
