027082e2345a6767363081ea81a31b8596697f84
nilesh-chothave <nileshchothave2003@gmail.com>
2026-08-04 21:23:47 +0530

    C++ Programming Practice Files Uploaded.

---
+++ Practice/program5.cpp
@@ -0,0 +1,28 @@
+/*
+    Algorithm
+
+    START
+        Accept first number as No1
+        Accept second number as No2
+        Perform addition of No1 & No2
+        Diplay the result
+    STOP
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    float i, j, k;
+
+    cout<<"Enter the first number : \n";
+    cin>>i;
+
+    cout<<"Enter the second number : \n";
+    cin>>j;
+
+    k = i + j;
+
+    cout<<"Addition of two number is : \n"<<k;
+}
