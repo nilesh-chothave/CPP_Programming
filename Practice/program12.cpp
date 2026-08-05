576a91f5271d328d8809fe85b3430e969e6dbb7d
nilesh-chothave <nileshchothave2003@gmail.com>
2026-08-05 12:04:21 +0530

    C++ Programming Practice Files Uploaded.

---
+++ Practice/program12.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+void CheckEvenOdd(int iNo)
+{
+    int iRemainder = 0;
+
+    iRemainder = iNo % 2;
+
+    if(iRemainder == 0)
+    {
+        cout<<"Number is Even \n";
+    }
+    else
+    {
+        cout<<"Number is Odd \n";
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    cout<<"Enter number : \n";
+    cin>>iValue;
+
+    CheckEvenOdd(iValue);
+
+    return 0;
+}
