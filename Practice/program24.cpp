83c538fda31231b9c43a4eb25eccd31ed4c7255e
nilesh-chothave <nileshchothave2003@gmail.com>
2026-08-06 19:01:30 +0530

    C++ Programming Practice Files Uploaded.

---
+++ Practice/program24.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    //Sequance
+
+    cout<<"Jay Ganesh...\n";
+    cout<<"Jay Ganesh...\n";
+    cout<<"Jay Ganesh...\n";
+    cout<<"Jay Ganesh...\n";
+
+    return 0;
+}
