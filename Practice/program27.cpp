05e22af044b5e0676275f8a319835fc31c43d13b
nilesh-chothave <nileshchothave2003@gmail.com>
2026-08-06 19:02:03 +0530

    C++ Programming Practice Files Uploaded.

---
+++ Practice/program27.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int iCnt = 0;
+    int iValue = 0;
+
+    cout<<"Enter the frequency : ";
+    cin>>iValue;
+
+    for(iCnt = 1; iCnt <= iValue; iCnt++)
+    {
+        cout<<"Jay Ganesh...\n";
+    }
+
+    return 0;
+}
