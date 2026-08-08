917bee71fb31701b1cae09aa5ac77861887447ee
nilesh-chothave <nileshchothave2003@gmail.com>
2026-08-08 19:13:02 +0530

    C++ Programming Practice Files Uploaded.

---
+++ Practice/program44.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+class NumberX
+{
+    public:
+        static bool CheckDivisible(int iNo)
+        {
+            if((iNo % 3 == 0) && (iNo % 5 == 0))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+};
+
+int main()
+{
+    int iValue = 0;
+    bool bRet = false;
+
+    cout<<"Enter number : ";
+    cin>>iValue;
+
+    bRet = NumberX::CheckDivisible(iValue);
+
+    if(bRet)
+    {
+        cout<<"Number is divisible by 3 & 5 \n";
+    }
+    else
+    {
+        cout<<"Number is not divisible by 3 & 5 \n";
+    }
+
+    return 0;
+}
