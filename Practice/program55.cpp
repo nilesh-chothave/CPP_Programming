4333a6e49738886acc9a700c798924dd89a83489
nilesh-chothave <nileshchothave2003@gmail.com>
2026-08-29 23:13:19 +0530

    C++ Programming Practice Files Uploaded.

---
+++ Practice/program55.cpp
@@ -0,0 +1,29 @@
+// Input : 5
+// Output : 1 2 3 4 
+
+#include<iostream>
+using namespace std;
+
+void Display(int iNo)
+{
+    int iCnt = 0;
+
+    for(iCnt = 1; iCnt < iNo; iCnt = iCnt + 1)
+    {
+        cout<<iCnt<<"\t";
+    }
+
+    cout<<"\n";
+}
+
+int main()
+{
+    int iValue = 0;
+
+    cout<<"Enter number : \n";
+    cin>>iValue;
+
+    Display(iValue);
+
+    return 0;
+}
